0b15e98bfe044829e9542e028b5de61893837f8f
Hk19-maker <hk4161390@gmail.com>
2025-12-27 04:02:53 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@
 window.addEventListener('load', () => {
     // Keep splash visible for 2.5 seconds
     setTimeout(() => {
-        document.body.classList.add('loaded');
+        document.body.classList.add('loaded'); // triggers CSS to hide splash
     }, 2500); // 2500ms = 2.5 seconds
 });
